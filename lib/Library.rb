dab1fdec3d044e7d197b7869fe04e027211ca393
MEGApixel23 <megapixel23@ya.ru>
2015-05-15 12:02:58 +0300

    'Refacrot'

--- lib/Library.rb
+++ lib/Library.rb
@@ -10,24 +10,24 @@ require_relative "Library/classes/Reader"
 require_relative "Library/classes/Order"
 require_relative "Library/classes/Library"
 
+=begin
 author = Library::Author.new({'author' => 'Author', 'bio' => 'Bio'})
 author.save
-=begin
-book = Book.new({'title' => 'Title', 'author' => author})
+book = Library::Book.new({'title' => 'Title', 'author' => author})
 book.save
-reader = Reader.new({'name' => 'Name'})
+reader = Library::Reader.new({'name' => 'Name'})
 reader.save
-order = Order.new({'book' => book, 'reader' => reader})
+order = Library::Order.new({'book' => book, 'reader' => reader})
 order.save
 =end
 
 =begin
-order = Order.get_by_id 1
-author = Author.get_by_id 1
-book = Book.get_by_id 1
-reader = Reader.get_by_id 1
+order = Library::Order.get_by_id 1
+author = Library::Author.get_by_id 1
+book = Library::Book.get_by_id 1
+reader = Library::Reader.get_by_id 1
 
-library = Library.new
+library = Library::Library.new
 library << order
 library << author
 library << book
@@ -37,19 +37,19 @@ library.save
 =end
 
 =begin
-library = Library.get_by_id 2
-library << (Order.get_by_id 2)
+library = Library::Library.get_by_id 2
+library << (Library::Order.get_by_id 2)
 library.save
 print library.who_often_takes_book 3
 print library.most_popular_book
 =end
 
 =begin
-library = Library.get_by_id 2
+library = Library::Library.get_by_id 2
 print library.who_often_takes_book 3
 =end
 
 =begin
-library = Library.get_by_id 2
+library = Library::Library.get_by_id 2
 print library.most_popular_books_readers
 =end
